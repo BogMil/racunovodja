dbd7361437c1ab58d394dc76410c11c7de91b2e7
Milan Bogdanovic <milanbogdanovic11@hotmail.com>
2020-05-03 20:39:58 +0200

    init

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import LoginComponent from './components/auth/login/index';
 
 export default function Routes() {
   return (
     <App>
       <Switch>
         <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.HOME} component={HomePage} />
+        <PrivateRoute exact path={routes.HOME}>
+          <HomePage />
+        </PrivateRoute>
+        {/* <Route path={routes.HOME} component={HomePage} /> */}
+        <Route path={routes.LOGIN} component={LoginComponent} />
       </Switch>
     </App>
+  );
+}
+
+function PrivateRoute({ children, ...rest }: any) {
+  const isAuthenticated = false;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: routes.LOGIN,
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
