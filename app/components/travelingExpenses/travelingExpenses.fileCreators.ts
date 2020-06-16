c3ecdd9b7e51969f2a65c414c63a3d56f6bb3d8b
Milan Bogdanovic <milanbogdanovic11@hotmail.com>
2020-06-16 22:17:09 +0200

    generating xml ppp-pd

--- app/components/travelingExpenses/travelingExpenses.fileCreators.ts
+++ app/components/travelingExpenses/travelingExpenses.fileCreators.ts
@@ -4,7 +4,8 @@ import {
   PUTNI_TROSKOVI_DIR,
   PUTNI_TROSKOVI_PPP_PD_FILE
 } from '../../constants/files';
-import { Employee } from '../employees/types';
+import { EmployeeWithRelations, TravelingExpense, TravelingExpenseWithDetails, EmployeeTravelingExpenseCalculator } from './travelingExpenses.types';
+import { numberWithThousandSeparator } from '../../utils/numberWithThousandSeparator';
 var fs = require('fs');
 const { shell } = require('electron');
 var builder = require('xmlbuilder');
@@ -12,11 +13,11 @@ var builder = require('xmlbuilder');
 export function create_PPP_PD_File(
   year: number,
   month: number,
-  employees: Employee[]
+  travelingExpense: TravelingExpenseWithDetails
 ) {
 
   let filePath =create_PPP_PD_xmlFile(year,month);
-  let xmlContent=createXmlContent(year,month,employees);
+  let xmlContent=createXmlContent(year,month,travelingExpense);
   fs.writeFile(
     filePath,
     xmlContent,
@@ -26,7 +27,7 @@ export function create_PPP_PD_File(
   );
 }
 
-function createXmlContent(year:number,month:number,employees:Employee[]){
+function createXmlContent(year:number,month:number,travelingExpense:TravelingExpenseWithDetails){
 
   var xml = builder.create('tns:PodaciPoreskeDeklaracije');
     xml.att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
@@ -43,7 +44,7 @@ function createXmlContent(year:number,month:number,employees:Employee[]){
     let PodaciOIsplatiocu = xml.ele('tns:PodaciOIsplatiocu');
     PodaciOIsplatiocu.ele('tns:TipIsplatioca', 2);
     PodaciOIsplatiocu.ele('tns:PoreskiIdentifikacioniBroj', 101408574);
-    PodaciOIsplatiocu.ele('tns:BrojZaposlenih', employees.length);
+    PodaciOIsplatiocu.ele('tns:BrojZaposlenih', travelingExpense.employees_with_relation.length);
     PodaciOIsplatiocu.ele('tns:MaticniBrojisplatioca', '0825740');
     PodaciOIsplatiocu.ele('tns:NazivPrezimeIme', 'SSŠ Vasa Pelagić');
     PodaciOIsplatiocu.ele('tns:SedistePrebivaliste', 217);
@@ -53,67 +54,32 @@ function createXmlContent(year:number,month:number,employees:Employee[]){
 
     let DeklarisaniPrihodi = xml.ele('tns:DeklarisaniPrihodi');
 
-    employees.forEach((employee,i)=>{
-    let PodaciOPrihodima = DeklarisaniPrihodi.ele('tns:PodaciOPrihodima');
-    PodaciOPrihodima.ele('tns:RedniBroj', i+1);
-    PodaciOPrihodima.ele('tns:VrstaIdentifikatoraPrimaoca', 1);
-    PodaciOPrihodima.ele('tns:IdentifikatorPrimaoca', employee.jmbg);
-    PodaciOPrihodima.ele('tns:Prezime', employee.last_name);
-    PodaciOPrihodima.ele('tns:Ime', employee.first_name);
-    PodaciOPrihodima.ele('tns:OznakaPrebivalista', employee.municipality.code);
-    PodaciOPrihodima.ele('tns:SVP', 105602000);
-    PodaciOPrihodima.ele('tns:MesecniFondSati', 168);
-    PodaciOPrihodima.ele('tns:Bruto', 3378.0);
-    PodaciOPrihodima.ele('tns:OsnovicaPorez', 3378.0);
-    PodaciOPrihodima.ele('tns:Porez', 338.0);
-    PodaciOPrihodima.ele('tns:OsnovicaDoprinosi', '0.0');
-    PodaciOPrihodima.ele('tns:PIO', '0.0');
-    PodaciOPrihodima.ele('tns:ZDR', '0.0');
-    PodaciOPrihodima.ele('tns:NEZ', '0.0');
-    PodaciOPrihodima.ele('tns:PIOBen', '0.0');
-    PodaciOPrihodima.ele('tns:DeklarisaniMFP');
-    })
-    let PodaciOPrihodima1 = DeklarisaniPrihodi.ele('tns:PodaciOPrihodima');
+    travelingExpense.employees_with_relation.forEach((employeeWithRelations,i)=>{
+      let employeeTravelingExpenseCalculator=new EmployeeTravelingExpenseCalculator(travelingExpense.year,travelingExpense.month,travelingExpense.maxNonTaxedValue,travelingExpense.preracun_na_bruto,travelingExpense.stopa);
+      let calculation =employeeTravelingExpenseCalculator.getCalculation(employeeWithRelations);
 
-    PodaciOPrihodima1.ele('tns:RedniBroj', 1);
-    PodaciOPrihodima1.ele('tns:VrstaIdentifikatoraPrimaoca', 1);
-    PodaciOPrihodima1.ele('tns:IdentifikatorPrimaoca', '0204986865068');
-    PodaciOPrihodima1.ele('tns:Prezime', 'Kosovac');
-    PodaciOPrihodima1.ele('tns:Ime', 'Katarina');
-    PodaciOPrihodima1.ele('tns:OznakaPrebivalista', 217);
-    PodaciOPrihodima1.ele('tns:SVP', 105602000);
-    PodaciOPrihodima1.ele('tns:MesecniFondSati', 168);
-    PodaciOPrihodima1.ele('tns:Bruto', 3378.0);
-    PodaciOPrihodima1.ele('tns:OsnovicaPorez', 3378.0);
-    PodaciOPrihodima1.ele('tns:Porez', 338.0);
-    PodaciOPrihodima1.ele('tns:OsnovicaDoprinosi', '0.0');
-    PodaciOPrihodima1.ele('tns:PIO', '0.0');
-    PodaciOPrihodima1.ele('tns:ZDR', '0.0');
-    PodaciOPrihodima1.ele('tns:NEZ', '0.0');
-    PodaciOPrihodima1.ele('tns:PIOBen', '0.0');
-    PodaciOPrihodima1.ele('tns:DeklarisaniMFP');
+      let PodaciOPrihodima = DeklarisaniPrihodi.ele('tns:PodaciOPrihodima');
+      PodaciOPrihodima.ele('tns:RedniBroj', i+1);
+      PodaciOPrihodima.ele('tns:VrstaIdentifikatoraPrimaoca', 1);
+      PodaciOPrihodima.ele('tns:IdentifikatorPrimaoca', employeeWithRelations.employee.jmbg);
+      PodaciOPrihodima.ele('tns:Prezime', employeeWithRelations.employee.last_name);
+      PodaciOPrihodima.ele('tns:Ime', employeeWithRelations.employee.first_name);
+      PodaciOPrihodima.ele('tns:OznakaPrebivalista', employeeWithRelations.employee.municipality.code);
+      PodaciOPrihodima.ele('tns:SVP', 105602000);
+      // PodaciOPrihodima.ele('tns:MesecniFondSati', 168);
+      PodaciOPrihodima.ele('tns:Bruto', numberWithThousandSeparator(calculation.brutoOporezivo,2,''));
+      PodaciOPrihodima.ele('tns:OsnovicaPorez', numberWithThousandSeparator(calculation.oporezivo,2,''));
+      PodaciOPrihodima.ele('tns:Porez', numberWithThousandSeparator(calculation.porez,2,''));
+      PodaciOPrihodima.ele('tns:OsnovicaDoprinosi', '0.0');
+      PodaciOPrihodima.ele('tns:PIO', '0.0');
+      PodaciOPrihodima.ele('tns:ZDR', '0.0');
+      PodaciOPrihodima.ele('tns:NEZ', '0.0');
+      PodaciOPrihodima.ele('tns:PIOBen', '0.0');
+      PodaciOPrihodima.ele('tns:DeklarisaniMFP');
+    });
 
-    let PodaciOPrihodima2 = DeklarisaniPrihodi.ele('tns:PodaciOPrihodima');
 
-    PodaciOPrihodima2.ele('tns:RedniBroj', 2);
-    PodaciOPrihodima2.ele('tns:VrstaIdentifikatoraPrimaoca', 1);
-    PodaciOPrihodima2.ele('tns:IdentifikatorPrimaoca', 1705991862517);
-    PodaciOPrihodima2.ele('tns:Prezime', 'Čolaković');
-    PodaciOPrihodima2.ele('tns:Ime', 'Dušan');
-    PodaciOPrihodima2.ele('tns:OznakaPrebivalista', '022');
-    PodaciOPrihodima2.ele('tns:SVP', 101110000);
-    PodaciOPrihodima2.ele('tns:MesecniFondSati', 168);
-    PodaciOPrihodima2.ele('tns:Bruto', 2489);
-    PodaciOPrihodima2.ele('tns:OsnovicaPorez', 2489);
-    PodaciOPrihodima2.ele('tns:Porez', 249);
-    PodaciOPrihodima2.ele('tns:OsnovicaDoprinosi', 0);
-    PodaciOPrihodima2.ele('tns:PIO', 0);
-    PodaciOPrihodima2.ele('tns:ZDR', 0);
-    PodaciOPrihodima2.ele('tns:NEZ', 0);
-    PodaciOPrihodima2.ele('tns:PIOBen', 0);
-    PodaciOPrihodima2.ele('tns:DeklarisaniMFP');
-
-    let doc = xml.end({ pretty: true });
+    return xml.end({ pretty: true });
 }
 function create_PPP_PD_xmlFile(year:number,month:number){
   if (!fs.existsSync(`C:\\${ROOT_DIR}`)) fs.mkdir(`C:\\${ROOT_DIR}`, () => {});
