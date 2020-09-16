72e480e0b86cda337913923d76325859c722f380
BogMil <milanbogdanovic11@hotmail.com>
2020-09-16 23:30:03 +0200

    fixing home

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import routes from '../constants/routes.json';
 
 export default function Home() {
   return (
-    <div>
-      <h2>Dobrodošli u informacioni sistem "Računovođa"</h2>
-    </div>
+    <Container className="noselect">
+      <Row style={{ marginTop: 10 }} className={`justify-content-md-center`}>
+        <Col>
+          <h2>Dobrodošli u informacioni sistem "Računovođa"!</h2>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 50 }}>
+        <Col className="d-flex justify-content-center">
+          <NavLink to={routes.DOSTAVLJAC_MAILOVA}>
+            <div className="d-flex justify-content-center">
+              <Button>
+                <i className="fa fa-paper-plane" />
+              </Button>
+            </div>
+            <div className="d-flex justify-content-center">
+              <h3>Mail Dostavljač</h3>
+            </div>
+          </NavLink>
+        </Col>
+      </Row>
+    </Container>
   );
 }
